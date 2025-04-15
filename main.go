93ed0e8b0950588e0e4bd220121a434db3e4e84b
shayan15sa <shay.sale86@gmail.com>
2025-04-15 13:38:36 +0330

    Revert "added camera functionality in sim mode"
    This reverts commit 7264bee21f73a4cfdb537fa4c1a02dd17ca4c8f2.
    
    opssies

--- main.go
+++ main.go
@@ -50,14 +50,12 @@ func main() {
 				fmt.Println("mode changing")
 			}
 		case Simulating:
-			rl.BeginMode2D(camera)
 			updatePlanets(planets)
 			DrawPlanets(planets)
 			cameraControl(&camera)
 			if rl.IsKeyPressed(rl.KeyP) {
 				currentMode = Editing
 			}
-			rl.EndMode2D()
 		}
 		// Begin drawing
 
